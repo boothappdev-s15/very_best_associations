9f59cf4b109cb6e3d3440b440fee879ad56b0b1d
Jgoodrich07 <Jeromegoodrich@gmail.com>
2015-05-21 22:10:55 -0500

    Through pt. 4 of the homework

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,2 +1,9 @@
 class Neighborhood < ActiveRecord::Base
+
+has_many :venue
+
+validates :name, :presence => true
+validates :name, :uniqueness => {:scope => :city}
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+
+has_many :favorites
+
+validates :username, :presence => true, :uniqueness => true
+
 end
