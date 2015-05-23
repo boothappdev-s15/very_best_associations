cb00c59b5c84cfc8a4867be2a1778077f3540102
np1989 <np1989@gmail.com>
2015-05-23 14:10:48 -0500

    Phase 1 - Validations

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,2 +1,7 @@
 class Dish < ActiveRecord::Base
+
+  validates :name, :presence => true , :uniqueness => true
+  validates :cusine_id, :presence => true
+
+
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,2 +1,9 @@
 class Favorite < ActiveRecord::Base
+
+validates :user_id, :presence => true
+validates :dish_id, :presence => true , :uniqueness => { :scope => :user_id }
+
+validates :venue_id, :presence => true
+
+
 end
