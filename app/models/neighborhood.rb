7082dea6a7392523c9588515288271dc749fb43d
Elias Stambolis <estambol@chicagobooth.edu>
2015-05-25 18:22:40 -0500

    validations appear complete

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,2 +1,4 @@
 class Neighborhood < ActiveRecord::Base
+validates :name, :presence => true
+validates :name, :uniqueness => { :scope => :city}
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,2 +1,4 @@
 class Venue < ActiveRecord::Base
+  validates :name, :presence => true
+  validates :neighborhood_id, :presence => true
 end
