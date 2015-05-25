a8e884a08c0832b44073ea0d2f2bd5e7fa7691c8
mpoelst <mpoelstr@chicagobooth.edu>
2015-05-25 15:29:30 -0500

    Associations Updatess

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,6 @@
 class Venue < ActiveRecord::Base
   validates :name, :presence => :unique, :uniqueness => { :scope => :address}
   validates :neighborhood_id, :presence => true
+  belongs_to :neighborhood
+  has_many :favorites
 end
