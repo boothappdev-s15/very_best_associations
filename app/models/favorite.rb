edd57a94c89926448bff44477d26819eec58fb68
edombrowski <Katie@Katies-MacBook-Pro-3.local>
2015-05-25 22:06:25 -0500

    Part 5 complete
    Errors on Dishes#Index and Venues#Index…asked question on First Draft
    for help

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,5 +2,6 @@ class Favorite < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :dish
+  belongs_to :venue
 
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,11 +1,8 @@
 class Venue < ActiveRecord::Base
 
-  validates :name, :uniqueness => { :scope => :address }
-  validates :name, :presence => true
+  validates :name, :presence => true, :uniqueness => { :scope => :address }
   validates :neighborhood_id, :presence => true
 
   belongs_to :neighborhood
   has_many :favorites
-  belongs_to :user
-
 end
