1e8faba004f35c6546421a71766ef19e0948a6b0
edombrowski <Katie@Katies-MacBook-Pro-3.local>
2015-05-25 20:54:15 -0500

    part 2 complete
    added associations and validations

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,2 +1,11 @@
 class Venue < ActiveRecord::Base
+
+  validates :name, :uniqueness => { :scope => :address }
+  validates :name, :presence => true
+  validates :neighborhood_id, :presence => true
+
+  belongs_to :neighborhood
+  has_many :favorites
+  belongs_to :user
+
 end
