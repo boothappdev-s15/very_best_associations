40c8d676df52219fdf07a6282d0d7d30f620c4d8
rohitmenon83 <rohitmenon83@yahoo.com>
2015-05-26 15:27:41 -0500

    Part IV-VI
    Completed the HW

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,4 +2,5 @@ class Neighborhood < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => { :scope => :city}, :uniqueness=> {:case_sensitive => false}
 
   has_many :venues
+  has_many :favorites, :through => :venues
 end
