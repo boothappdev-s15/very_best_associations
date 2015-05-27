c8bb7e3be539f7031b2240546893a989460b1dc0
michaelhf <michaelhf@gmail.com>
2015-05-27 14:28:10 -0500

    part 4

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,2 +1,4 @@
 class Dish < ActiveRecord::Base
+      validates :name, :presence => true
+      belongs_to :cuisine
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,2 +1,4 @@
 class Venue < ActiveRecord::Base
+  validates :name, :uniqueness => {:scope=>:address}
+  belongs_to :neighborhood
 end
