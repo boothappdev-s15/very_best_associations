0f3b5a344370966297510769440c5a7846133033
Dave Newberry <david.newberry@gmail.com>
2015-05-26 22:37:09 -0500

    added validations

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,2 +1,6 @@
 class Neighborhood < ActiveRecord::Base
+
+  validates :name, presence: true, uniqueness: { scope: :city, message: "neighborhoods should be unique in cities", case_sensitive: false}
+
+
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,2 +1,4 @@
 class Venue < ActiveRecord::Base
+  validates :name, :presence true, :uniqueness {:scope :address, :message "venues should have unique address"}
+  validates :neighborhood_id, :presence true
 end
