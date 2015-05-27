e89ffd837ef583f3e3cc67affd4157afa9924f52
Dave Newberry <david.newberry@gmail.com>
2015-05-27 00:48:34 -0500

    optional

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -3,7 +3,7 @@ class Neighborhood < ActiveRecord::Base
   validates :name, presence: true, uniqueness: { scope: :city, message: "neighborhoods should be unique in cities", case_sensitive: false}
 
   has_many :venues
-  # has_many :favorites, through: :venues
+  has_many :favorites, through: :venues
 
 
 end
