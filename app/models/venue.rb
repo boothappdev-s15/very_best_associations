a1471996ef3d75246f3557c6aa072a029abac8ee
AshleyHawn <ahawn@chicagobooth.edu>
2015-05-27 15:49:43 -0500

    HW week 8

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,10 +1,10 @@
 class Venue < ActiveRecord::Base
 
-  validates :name, :presence => true
-
-  validates :neighborhood_id, :presence => true
+  validates :name, :presence => true, :uniqueness => { :scope => :address }
 
   belongs_to :neighborhood
 
   has_many :favorites
+
+
 end
