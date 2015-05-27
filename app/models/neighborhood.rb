26f387300d65307074e1f8ca6877b540dcd49f0b
Dave Newberry <david.newberry@gmail.com>
2015-05-26 23:03:00 -0500

    added associations

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,5 +2,7 @@ class Neighborhood < ActiveRecord::Base
 
   validates :name, presence: true, uniqueness: { scope: :city, message: "neighborhoods should be unique in cities", case_sensitive: false}
 
+  has_many :venues
+
 
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,8 @@
 class Venue < ActiveRecord::Base
-  validates :name, :presence true, :uniqueness {:scope :address, :message "venues should have unique address"}
-  validates :neighborhood_id, :presence true
+  validates :name, :presence true,:uniqueness {:scope :address,:message "venues should have unique address"}
+  validates :neighborhood_id,:presence true
+
+  belongs_to  :neighborhood
+  has_many    :favorites
+
 end
