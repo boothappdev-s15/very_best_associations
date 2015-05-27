d60d424c5812181adf2c1b92eb93e2c67cc90af3
kfnasseri <kendra@chicagobooth.edu>
2015-05-27 14:13:43 -0500

    Finished

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,2 +1,10 @@
 class Favorite < ActiveRecord::Base
+# These validates were in the solutions but not in the homework instructions...
+  validates :user_id, :presence => true
+  validates :dish_id, :presence => true
+  validates :venue_id, :presence => true
+
+  belongs_to :user
+  belongs_to :venue
+  belongs_to :dish
 end
